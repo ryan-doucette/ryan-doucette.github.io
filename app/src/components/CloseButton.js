180dc5201bd0c0c8c2e2afd80778e7de1ee00f71
ryan-doucette <doucette.ry@northeastern.edu>
2022-02-18 19:20:56 -0500

    base done for menu modal

--- app/src/components/CloseButton.js
+++ app/src/components/CloseButton.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './styles/CloseButton.css'
 
 const CloseButton = () => {
+    const [hovering, setHovering] = useState(false);
 
     return (
-        <div className="closeButton">
-
+        <div className="closeButton" onMouseEnter ={() => setHovering(true)} onMouseLeave = {() => setHovering(false)} id={hovering ? 'hovering' : null}>
+            <div className="leftright"/>
+            <div className="rightleft"/>
         </div>
     );
 }
