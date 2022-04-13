e7e720c8469b698d079711c5223c88c4415e5295
ryan-doucette <doucette.ry@northeastern.edu>
2022-04-13 11:02:04 -0400

    added text animations to the greeting text

--- app/src/components/screens/home/Greeting.tsx
+++ app/src/components/screens/home/Greeting.tsx
@@ -6,9 +6,9 @@ const Greeting = () => {
 
   return (
     <div className="greeting">
-      <h1>Hey, I'm </h1><h1 className="highlightText">Ryan</h1>
+      <h1 className="greetingLetter">H</h1><h1 className="greetingLetter">e</h1><h1 className="greetingLetter">y</h1><h1 className="greetingLetter">,</h1><h1> </h1><h1 className="greetingLetter">I</h1><h1 className="greetingLetter">'</h1><h1 className="greetingLetter">m</h1><h1> </h1><h1 className="highlightText greetingLetter">R</h1><h1 className="highlightText greetingLetter">y</h1><h1 className="highlightText greetingLetter">a</h1><h1 className="highlightText greetingLetter">n</h1>
       <br/>
-      <h2 className="highlightText">Welcome</h2><h2> to my website</h2>
+      <h2 className="highlightText greetingLetter">W</h2><h2 className="highlightText greetingLetter">e</h2><h2 className="highlightText greetingLetter">l</h2><h2 className="highlightText greetingLetter">c</h2><h2 className="highlightText greetingLetter">o</h2><h2 className="highlightText greetingLetter">m</h2><h2 className="highlightText greetingLetter">e</h2><h2> </h2><h2 className="greetingLetter">t</h2><h2 className="greetingLetter">o</h2><h2> </h2><h2 className="greetingLetter">m</h2><h2 className="greetingLetter">y</h2><h2> </h2><h2 className="greetingLetter">w</h2><h2 className="greetingLetter">e</h2><h2 className="greetingLetter">b</h2><h2 className="greetingLetter">s</h2><h2 className="greetingLetter">i</h2><h2 className="greetingLetter">t</h2><h2 className="greetingLetter">e</h2>
     </div>
   );
 }
